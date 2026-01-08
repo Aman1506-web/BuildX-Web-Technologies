21b2795d4677aec4f8b9682f24abec395f197449
Aman Sharma <sharmaa1518@gmail.com>
2026-01-08 21:32:33 +0530

    Added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${spaceGrotesk.variable} antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
